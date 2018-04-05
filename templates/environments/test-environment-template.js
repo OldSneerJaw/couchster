cca8ec03fac06df3bc1fc8a600598eb816318aba
Joel Andrews <oldsneerjaw@gmail.com>
2018-04-04 19:23:08 -0700

    Issue #11: Generate a new test environment for each test-fixture-maker test case

--- templates/environments/test-environment-template.js
+++ templates/environments/test-environment-template.js
@@ -2,8 +2,11 @@ function makeTestEnvironment(simpleMock) {
   // CouchDB utility functions
   const isArray = Array.isArray;
   const log = simpleMock.stub();
-  const sum = (list) => list.reduce((accumulator, item) => accumulator + item, 0);
   const toJSON = JSON.stringify;
+
+  function sum(list) {
+    return list.reduce((accumulator, item) => accumulator + item, 0);
+  }
 
   return {
     isArray,
--- templates/environments/validation-environment-template.js
+++ templates/environments/validation-environment-template.js
@@ -9,8 +9,11 @@ function makeValidationEnvironment(simpleMock) {
   // CouchDB utility functions
   const isArray = Array.isArray;
   const log = simpleMock.stub();
-  const sum = (list) => list.reduce((accumulator, item) => accumulator + item, 0);
   const toJSON = JSON.stringify;
+
+  function sum(list) {
+    return list.reduce((accumulator, item) => accumulator + item, 0);
+  }
 
   return {
     newDoc,
