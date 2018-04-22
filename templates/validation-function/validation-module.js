e6bcb8b9ee0182987cb10f61013afc4553c7140e
Joel Andrews <oldsneerjaw@gmail.com>
2018-04-22 14:32:43 -0700

    Issue #16: Minor refactoring of document validation modules

--- templates/validation-function/validation-module.js
+++ templates/validation-function/validation-module.js
@@ -1,26 +1,26 @@
 function validationModule(utils, simpleTypeFilter, typeIdValidator) {
-  var documentPropertiesValidationModule =
-    importValidationFunctionFragment('document-properties-validation-module.js')(utils, simpleTypeFilter, typeIdValidator);
   var documentConstraintsValidationModule =
     importValidationFunctionFragment('document-constraints-validation-module.js')(utils);
+  var documentPropertiesValidationModule =
+    importValidationFunctionFragment('document-properties-validation-module.js')(utils, simpleTypeFilter, typeIdValidator);
 
-  return { validateDoc: validateDoc };
+  return {
+    validateDoc: function validateDoc(newDoc, oldDoc, userContext, securityInfo, docDefinition, docType) {
+      var documentConstraintValidationErrors =
+        documentConstraintsValidationModule.validateDocument(newDoc, oldDoc, docDefinition);
 
-  function validateDoc(newDoc, oldDoc, userContext, securityInfo, docDefinition, docType) {
-    var documentConstraintValidationErrors =
-      documentConstraintsValidationModule.validateDocument(newDoc, oldDoc, userContext, securityInfo, docDefinition);
+      // Only validate the document's contents if it's being created or replaced. There's no need if it's being deleted.
+      var propertyConstraintValidationErrors = !newDoc._deleted ?
+        documentPropertiesValidationModule.validateProperties(newDoc, oldDoc, userContext, securityInfo, docDefinition) :
+        [ ];
 
-    // Only validate the document's contents if it's being created or replaced. There's no need if it's being deleted.
-    var propertyConstraintValidationErrors = !newDoc._deleted ?
-      documentPropertiesValidationModule.validateProperties(newDoc, oldDoc, userContext, securityInfo, docDefinition) :
-      [ ];
+      var validationErrors = documentConstraintValidationErrors.concat(propertyConstraintValidationErrors);
 
-    var validationErrors = documentConstraintValidationErrors.concat(propertyConstraintValidationErrors);
-
-    if (validationErrors.length > 0) {
-      throw { forbidden: 'Invalid ' + docType + ' document: ' + validationErrors.join('; ') };
-    } else {
-      return true;
+      if (validationErrors.length > 0) {
+        throw { forbidden: 'Invalid ' + docType + ' document: ' + validationErrors.join('; ') };
+      } else {
+        return true;
+      }
     }
-  }
+  };
 }
